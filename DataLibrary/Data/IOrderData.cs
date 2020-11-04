6cc8b8386278a6b1498a2a5f203081aa8e5cc9c2
weiping97 <weipingyap97@gmail.com>
2020-11-04 16:41:03 +0800

    setup up data access for the db

---
+++ DataLibrary/Data/IOrderData.cs
@@ -0,0 +1,13 @@
+﻿using DataLibrary.Models;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Data
+{
+    public interface IOrderData
+    {
+        Task<int> CreateOrder(OrderModel order);
+        Task<int> DeleteOrder(int orderId);
+        Task<OrderModel> GetOrderById(int orderId);
+        Task<int> UpdateOrder(int orderId, string orderName);
+    }
+}
---
+++ DataLibrary/Models/OrderModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class OrderModel
+    {
+        //decorations usually done on frontend
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters")]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        [DisplayName("Meal")]
+        [Range(1, int.MaxValue, ErrorMessage = "You need to select a meal from the list")]       
+        public int FoodId { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "You can select up to 10 meals")]
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
